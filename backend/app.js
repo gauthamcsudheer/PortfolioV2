655c61adfffc2c61bdd32f6c20927a8de7a859ec
Gautham C Sudheer <csgautham091003@gmail.com>
2026-03-03 20:15:56 +0530

    Fix: CORS issue

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,9 @@ app.use(morgan('dev'));
 app.use(cors({
   origin: [
     'http://localhost:5173', 
-    'https://portfolio-v2-ashy-two-56.vercel.app' // Add your actual Vercel URL here
+    'https://portfolio-v2-ashy-two-56.vercel.app',
+    'https://gauthamcsudheer.com',
+    'https://www.gauthamcsudheer.com'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'x-auth-token']
